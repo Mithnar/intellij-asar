7f9ee4c0f256ae5c091bb0984de627df92b7cc89
Mithnar <mithnar333@googlemail.com>
2026-03-30 21:15:07 +0200

    adjust LabelAnnotator comments to reflect implementation

--- src/main/kotlin/de/mithnar/plugin/asarasm/annotator/AsarLabelAnnotator.kt
+++ src/main/kotlin/de/mithnar/plugin/asarasm/annotator/AsarLabelAnnotator.kt
@@ -10,29 +10,37 @@ import de.mithnar.plugin.asarasm.psi.*
 
 /**
  *  Expected structures:
- *  1. isLabelDefinition:
+ *  1. isLabel:
  *  AsarLabelDefinitionImpl(LABEL_DEFINITION) ← matching element
+ *  AsarLabelReferenceImpl(LABEL_REFERENCE) ← matching element
  *
  *  Grammar rule:
  *  labelDefinition ::= LABEL_TOKEN
  *
- *  2. isAnonymousLabelDefinition / isAnonymousLabelReference:
+ *  2. isLocalLabel:
+ *  AsarLocalLabelDefinitionImpl(LOCAL_LABEL_DEFINITION) ← matching element
+ *  AsarLocalLabelReferenceImpl(LOCAL_LABEL_REFERENCE) ← matching element
+ *
+ *  Grammar rule:
+ *  labelDefinition ::= LABEL_TOKEN
+ *
+ *  3. isAnonymousLabelDefinition / isAnonymousLabelReference:
  *  AsarAnonymousLabelDefinitionImpl(ANONYMOUS_LABEL_DEFINITION) ← matching element
- *  AsarAnonymousLabelReferenceImpl(ANONYMOUS_LABEL_REFERENCE)   ← matching element
+ *  AsarAnonymousLabelReferenceImpl(ANONYMOUS_LABEL_REFERENCE) ← matching element
  *
  *  Grammar rule:
  *  anonymousLabelDefinition ::= plusLabel | minusLabel
- *  anonymousLabelReference  ::= plusLabel | minusLabel
+ *  anonymousLabelReference ::= plusLabel | minusLabel
  *
- *  3. isSymbolReferenceTarget:
+ *  4. isSymbolReferenceTarget:
  *  AsarSymbolReferenceTargetImpl(SYMBOL_REFERENCE_TARGET) ← matching element
  *
  *  Grammar rule:
- *  branchTarget         ::= symbolReferenceTarget | anonymousLabelReference
- *  jumpTarget           ::= symbolReferenceTarget | expressionTarget
+ *  branchTarget ::= symbolReferenceTarget | anonymousLabelReference
+ *  jumpTarget ::= symbolReferenceTarget | expressionTarget
  *  symbolReferenceTarget ::= IDENTIFIER_TOKEN | CONSTANT_TOKEN
  *
- *  4. isLabelReferenceInOperand
+ *  5. isLabelReferenceInOperand
  *  AsarPrimaryExpressionImpl
  *      PsiElement(IDENTIFIER_TOKEN)('MyTable') <- matching element
  *
@@ -50,12 +58,12 @@ import de.mithnar.plugin.asarasm.psi.*
 class AsarLabelAnnotator : Annotator {
 
     override fun annotate(el: PsiElement, holder: AnnotationHolder) {
-        val isLabelDefinition = el is AsarLabelDefinition || el is AsarLabelReference
+        val isLabel = el is AsarLabelDefinition || el is AsarLabelReference
         val isLocalLabel = el is AsarLocalLabelDefinition || el is AsarLocalLabelReference
         val isAnonymousLabel = el is AsarAnonymousLabelDefinition || el is AsarAnonymousLabelReference
         val isSymbolReferenceTarget = el is AsarSymbolReferenceTarget
 
-        if (isLabelDefinition || isLocalLabel || isAnonymousLabel || isSymbolReferenceTarget || isLabelReferenceInOperand(el)) {
+        if (isLabel || isLocalLabel || isAnonymousLabel || isSymbolReferenceTarget || isLabelReferenceInOperand(el)) {
             return holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
                 .range(el.textRange)
                 .textAttributes(AsarSyntaxHighlighter.LABEL)
@@ -63,7 +71,7 @@ class AsarLabelAnnotator : Annotator {
         }
     }
 
-    // Case 4
+    // Case 5
     private fun isLabelReferenceInOperand(el: PsiElement): Boolean {
         // Needs to be an identifier
         if (el.node.elementType != AsarTypes.IDENTIFIER_TOKEN) return false
