02142fcfbf09cce9f76e4739a013507052c97048
Mithnar <mithnargh@pm.me>
2026-05-28 02:59:33 +0200

    Fix incsrc+incbin for unquoted file paths

--- src/main/kotlin/de/mithnar/plugin/asarasm/psi/mixin/AsarIncludeDirectiveMixin.kt
+++ src/main/kotlin/de/mithnar/plugin/asarasm/psi/mixin/AsarIncludeDirectiveMixin.kt
@@ -1,25 +1,31 @@
+
 package de.mithnar.plugin.asarasm.psi.mixin
 
 import com.intellij.extapi.psi.ASTWrapperPsiElement
 import com.intellij.lang.ASTNode
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiReference
-import com.intellij.psi.tree.TokenSet
 import de.mithnar.plugin.asarasm.psi.AsarTypes
 import de.mithnar.plugin.asarasm.reference.AsarIncludePsiReference
 
 abstract class AsarIncludeDirectiveMixin(node: ASTNode) : ASTWrapperPsiElement(node) {
 
     override fun getReferences(): Array<PsiReference> {
-        val tokens = node.getChildren(TokenSet.create(AsarTypes.IDENTIFIER_TOKEN, AsarTypes.STRING_TOKEN))
-        if (tokens.size != 2) return PsiReference.EMPTY_ARRAY
+        val filePathNode = node.findChildByType(AsarTypes.FILE_PATH) ?: return PsiReference.EMPTY_ARRAY
+        val directiveStart = textRange.startOffset
 
-        val filenameToken = tokens[1]
-        val range = filenameToken.textRange.shiftLeft(textRange.startOffset)
-        val filenameRange = if (filenameToken.elementType == AsarTypes.STRING_TOKEN) {
-            range.cutOut(TextRange(1, filenameToken.text.length - 1))
+        val filePathRange = filePathNode.textRange.shiftLeft(directiveStart)
+
+        // If the path is a STRING_TOKEN, strip surrounding quotes.
+        val firstChild = filePathNode.firstChildNode
+        val filenameRange = if (
+            firstChild != null
+            && firstChild === filePathNode.lastChildNode
+            && firstChild.elementType == AsarTypes.STRING_TOKEN
+        ) {
+            filePathRange.cutOut(TextRange(1, firstChild.textLength - 1))
         } else {
-            range
+            filePathRange
         }
 
         return arrayOf(AsarIncludePsiReference(this, filenameRange))
